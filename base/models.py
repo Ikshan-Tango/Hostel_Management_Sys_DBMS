0cc0780f308423365663618e7b79fd9c84376923
ikshan-Tango <ikshanbhardwaj.2003@gmail.com>
2023-03-03 00:40:12 +0530

    added student model

--- base/models.py
+++ base/models.py
@@ -3,14 +3,24 @@ from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
+from django.conf import settings
+from phonenumber_field.modelfields import PhoneNumberField
+
+from string import ascii_uppercase
+
+
 from .managers import CustomUserManager
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_("email address"), unique=True)
+
+    is_student   = models.BooleanField(default=False)
+    is_caretaker   = models.BooleanField(default=False)
+    is_warden   = models.BooleanField(default=False)
+
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
-    date_joined = models.DateTimeField(default=timezone.now)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
@@ -19,3 +29,21 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+    
+class Student(models.Model):
+
+    blank_choice = [('', 'Choose'),]
+    hostels      = [("Hostel " + h, "Hostel " + h) for h in ascii_uppercase[:15]]
+
+    student    = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    college_id = models.CharField(primary_key = True, max_length=20)
+    name       = models.CharField(max_length=50)
+    age        = models.PositiveIntegerField()
+    room_num   = models.CharField(max_length=10)
+    hostel     = models.CharField(max_length=10, choices=blank_choice + hostels)
+    mobile_num = PhoneNumberField(null=False, blank=True, unique=True)
+    branch      = models.CharField(max_length=20, null=True)
+    year        = models.PositiveIntegerField(null=True)
+
+    def __str__(self) -> str:
+        return f"{self.college_id} {self.name}"
--- hostelM/settings.py
+++ hostelM/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
 
     'base.apps.BaseConfig',
     'rest_framework',
+    # Other apps…
+    "phonenumber_field",
 ]
 
 MIDDLEWARE = [
